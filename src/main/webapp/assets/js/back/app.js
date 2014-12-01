49ba99b08295005c3f249664ef690482c4ef9134
Lenin Hasda <lenin13bc@gmail.com>
2014-12-01 15:33:09 +0600

    fixed git ignore files

---
+++ src/main/webapp/assets/js/back/app.js
@@ -0,0 +1,34 @@
+$(function(){
+    // sidebar menu toggle
+	$('ul.nav li.hasSub').on('click', function(e){
+		// e.preventDefault();
+		if(!$(this).hasClass('active')) {
+			//$('ul.nav li').removeClass('active');
+			$(this).addClass('active');
+			$('ul.sub-nav').slideUp();
+			$(this).children('ul.sub-nav').slideDown();
+		}
+	});
+
+    // profile page datepickter
+    $('input#inputDateOfBirth').datepicker({dateFormat: 'dd-mm-yy'});
+
+    // ajax on message
+   /* $('.inbox li.list-group-item a').on('click', function(e){
+        e.preventDefault();
+
+        var ajaxURL = $(this).attr('href');
+
+        $.ajax({
+            type: 'POST',
+            url: ajaxURL+"&ajax=true",
+            dataType: 'json',
+            success: function(data){
+//                $('.panel-body').text(data);
+                console.log("hey lenin");
+                console.log(data);
+
+            }
+        });
+    });*/
+});
